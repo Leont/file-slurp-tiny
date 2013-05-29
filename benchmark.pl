b81805144f9b65ee70ccf97a2ad506b268e4a346
Leon Timmermans <fawaka@gmail.com>
2013-05-29 16:05:14 +0200

    Added more benchmarks

--- benchmark.pl
+++ benchmark.pl
@@ -2,16 +2,46 @@
 
 use strict;
 use warnings;
-use Benchmark 'cmpthese';
+use Benchmark ':hireswallclock', 'cmpthese';
 use File::Slurp;
 use File::Slurp::Tiny;
 
 my $filename = shift or die "No argument given";
 my $count = shift || 100;
 
+print "Slurping into a scalar\n";
 cmpthese($count, {
-	'POSIX'      => sub { open my $fh, '<', $filename or die "Couldn't open: $!"; POSIX::read fileno $fh, my $buffer, -s $fh },
+	'Slurp'      => sub { my $content = File::Slurp::read_file($filename) },
+	'Slurp-Tiny' => sub { my $content = File::Slurp::Tiny::read_file($filename) },
+});
+
+print "\nSlurping into a scalarref\n";
+cmpthese($count, {
 	'Slurp'      => sub { File::Slurp::read_file($filename, buffer_ref => \my $buffer) },
 	'Slurp-Tiny' => sub { File::Slurp::Tiny::read_file($filename, buffer_ref => \my $buffer) },
+});
+
+print "\nSlurping into an arrayref\n";
+cmpthese($count, {
+	'Slurp'      => sub { my $lines = File::Slurp::read_file($filename, { array_ref => 1 }) },
+	'Slurp-Tiny' => sub { File::Slurp::Tiny::read_lines($filename, array_ref => \my @lines) },
+});
+
+print "\nSlurping into an arrayref, chomped\n";
+cmpthese($count, {
+	'Slurp'       => sub { my $lines = File::Slurp::read_file($filename, array_ref => 1, chomp => 1) },
+	'Slurp-Tiny'  => sub { File::Slurp::Tiny::read_lines($filename, array_ref => \my @lines, chomp => 1) },
+});
+
+print "\nSlurping into an array\n";
+cmpthese($count, {
+	'Slurp'       => sub { my @lines = File::Slurp::read_file($filename) },
+	'Slurp-Tiny'  => sub { my @lines = File::Slurp::Tiny::read_lines($filename) },
+});
+
+print "\nSlurping into an array, chomped\n";
+cmpthese($count, {
+	'Slurp'       => sub { my @lines = File::Slurp::read_file($filename, chomp => 1) },
+	'Slurp-Tiny'  => sub { my @lines = File::Slurp::Tiny::read_lines($filename, chomp => 1) },
 });
 
